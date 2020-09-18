7f9e510de693dee88c64b8835d624c3b5f7772b1
mustbebuilt <martin@mustbebuilt.co.uk>
2020-09-18 08:31:38 +0100

    Amended ObjectID

--- routes/routes.js
+++ routes/routes.js
@@ -5,8 +5,9 @@ const router = express.Router();
 module.exports = (app) => {
 
     router.get('/film/:filmID', (req, res) => {
-        let filmID = parseInt(req.params.filmID);
-        app.set('myDb').collection('filmsCollection').find({"filmID": filmID}).toArray(function(err, docs) {
+        let filmID = req.params.filmID;
+        var o_id = new ObjectId(filmID);
+        app.set('myDb').collection('filmsCollection').find({"_id": o_id}).toArray(function(err, docs) {
             if (err) {
                 console.error(err)
             }
@@ -20,50 +21,8 @@ module.exports = (app) => {
      
     // add RESTful POST, PUT and DELETE Routes
 
-
-
-
-
   
     return router;
 
 }
 
-
-
-// var express = require('express');
-// var router = express.Router();
-
-// router = function(app){
-//     app.route('/film')
-//     .get(function (req, res) {
-//         //console.dir(req.body)
-//         res.send(`
-//         GET good: ${req.method}
-//         Data: ${req.body}
-//         `); 
-//     })
-//     .post(function (req, res) {
-//         //console.dir(req.body)
-//         //res.send(req.body)
-//         //res.send(`POST good: ${req.method}`);
-//         res.json(req.body);
-//     })
-//     .put(function (req, res) {
-//         res.send('PUT good')
-//     })
-//     .delete(function (req, res) {
-//         es.send('Delete good')
-//     });
-
-//     //GET PARAMETER
-//     app.route('/film/:filmID')
-//     .get(function (req, res) {
-//         res.send(`
-//         GET good: ${req.method}
-//         Data: ${req.params.filmID}
-//         `); 
-//     })
-// }
-
-// module.exports = router;
